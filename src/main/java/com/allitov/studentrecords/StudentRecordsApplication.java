3d0ec4b45b9868b7a95725116c989558352d2736
allitov <litovkinalexan@gmail.com>
2023-11-03 13:37:27 +0700

    Init commit

---
+++ src/main/java/com/allitov/studentrecords/StudentRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.allitov.studentrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRecordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allitov/studentrecords/StudentRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allitov.studentrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRecordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
