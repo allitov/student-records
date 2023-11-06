5271363af414248e67aafad13ae347ad4faed7dc
allitov <litovkinalexan@gmail.com>
2023-11-06 22:33:32 +0700

    Add values to shell commands

--- src/main/java/com/allitov/studentrecords/ui/StudentRecordsUI.java
+++ src/main/java/com/allitov/studentrecords/ui/StudentRecordsUI.java
@@ -24,7 +24,7 @@ public class StudentRecordsUI {
     private final ApplicationEventPublisher publisher;
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
-    @ShellMethod(key = "p")
+    @ShellMethod(key = "p", value = "Print all students data from app memory to console.")
     public String printAllStudents() {
         List<Student> result = studentManager.getAllStudents();
 
@@ -32,7 +32,7 @@ public class StudentRecordsUI {
                 result.stream().map(Student::toString).collect(Collectors.joining(LINE_SEPARATOR));
     }
 
-    @ShellMethod(key = "a")
+    @ShellMethod(key = "a", value = "Add student data to app memory.")
     public String addStudent(@ShellOption(value = "ln") String lastName,
                              @ShellOption(value = "fn") String firstName,
                              int age) {
@@ -53,7 +53,7 @@ public class StudentRecordsUI {
         }
     }
 
-    @ShellMethod(key = "d")
+    @ShellMethod(key = "d", value = "Delete student data from app memory by id.")
     public String deleteStudentById(String id) {
         Optional<Student> result = studentManager.removeStudentById(id);
 
@@ -66,14 +66,14 @@ public class StudentRecordsUI {
         }
     }
 
-    @ShellMethod(key = "da")
+    @ShellMethod(key = "da", value = "Delete all students data from app memory.")
     public String deleteAllStudents() {
         boolean result = studentManager.removeAllStudents();
 
         return result ? "Все записи удалены." : "Что-то пошло не так.";
     }
 
-    @ShellMethod(key = "e")
+    @ShellMethod(key = "e", value = "Save students data to file and close app.")
     public void exit() {
         studentManager.serializeData();
         System.exit(0);
